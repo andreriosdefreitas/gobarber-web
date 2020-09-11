0a36a14096c101adf74857ef5e16791fce7f1408
André Rios <andre.rios.freitas@gmail.com>
2020-09-11 08:58:41 +0100

    GoBarber - toasts

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import GlobalStyle from './styles/global';
 import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import { AuthProvider } from './hooks/AuthContext';
+import ToasContainer from './components/ToastContainer';
 
 const App: React.FC = () => (
   <>
     <AuthProvider>
       <SignIn />
     </AuthProvider>
+    <ToasContainer />
     <GlobalStyle />
   </>
 );
